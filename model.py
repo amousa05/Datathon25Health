81d62ebc7516b2e6ff2d7dc11f5ce5401dbe1136
Phineas Luu <truongluu2703@gmail.com>
2025-11-22 23:40:14 -0500

    Add file (to be used)

---
+++ model.py
@@ -0,0 +1,11 @@
+# Import libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from math import radians, sin, cos, sqrt, atan2
+
+# Preprocessing
+from sklearn.preprocessing import MinMaxScaler
+
+from sklearn.neighbors import NearestNeighbors
+
